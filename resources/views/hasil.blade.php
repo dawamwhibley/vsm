a2923a3d4e46efc6fb618988bb7f8d73b2c02614
dawam <dawam@gaeasystem.com>
2020-10-19 15:49:19 +0700

    update no urut

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -11,16 +11,19 @@
                     <table class="table">
                         <thead>
                         <tr>
-                            <th>No Document</th>
+                            <th>No.</th>
+                            <th>No Dokumen</th>
                             <th>Judul</th>
                             <th>Ranking</th>
                             <th>Detail</th>
                         </tr>
                         </thead>
                         <tbody>
+                        @php($i = 1)
                         @foreach($hasil as $h)
                             @if($h['ranking']>0)
                                 <tr>
+                                    <td>{{$i++}}</td>
                                     <td>{{$h['id_doc']}}</td>
                                     <td>{{$h['judul']}}</td>
                                     <td>{{$h['ranking']}}</td>
